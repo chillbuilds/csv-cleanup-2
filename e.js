6eb5203ed9fb9008316f11119c11c7ce81e46108
chillbuilds <heliumface770@gmail.com>
2021-04-20 16:50:55 -0600

    generating error csv with skus

--- e.js
+++ e.js
@@ -1,8 +1,14 @@
 const fs = require('fs')
+const arrayUniq = require('array-uniq')
 const linesPer = 30
 const errArr = fs.readFileSync('./errors.csv', 'utf-8').split('\n\t')
+var errSkuArr = []
 var csvArr = []
 var csvContArr = []
+
+fs.writeFileSync('./output/good.csv', 'Handle,Title,Body (HTML),Vendor,Type,Tags,Published,Option1 Name,Option1 Value,Option2 Name,Option2 Value,Option3 Name,Option3 Value,Variant SKU,Variant Grams,Variant Inventory Tracker,Variant Inventory Qty,Variant Inventory Policy,Variant Fulfillment Service,Variant Price,Variant Compare At Price,Variant Requires Shipping,Variant Taxable,Variant Barcode,Image Src,Image Position\n')
+fs.writeFileSync('./output/bad.csv', 'Handle,Title,Body (HTML),Vendor,Type,Tags,Published,Option1 Name,Option1 Value,Option2 Name,Option2 Value,Option3 Name,Option3 Value,Variant SKU,Variant Grams,Variant Inventory Tracker,Variant Inventory Qty,Variant Inventory Policy,Variant Fulfillment Service,Variant Price,Variant Compare At Price,Variant Requires Shipping,Variant Taxable,Variant Barcode,Image Src,Image Position\n')
+fs.writeFileSync('./output/error-skus.csv', '')
 
 fs.readdirSync('./input').forEach(file => {
     csvArr.push(file)
@@ -32,8 +38,20 @@ for(var i = 0; i < csvContArr.length; i++){
     for(var j = 0; j < errArr.length; j++){
         for(var k = 0; k < csvContArr[i].urls.length; k++){
             if(csvContArr[i].urls[k] == errArr[j]){
-                console.log(csvContArr[i].sku)
+                errSkuArr.push(csvContArr[i].sku)
             }
+        }
+    }
+}
+errSkuArr = arrayUniq(errSkuArr)
+
+// loop through errSkuArr and deem each chunk as good or bad
+
+for(var i = 0; i < errSkuArr.length; i++){
+    for(var j = 0; j < csvContArr.length; j++){
+        if(errSkuArr[i] == csvContArr[j].sku){
+            fs.appendFileSync('./output/bad.csv', csvContArr[j].chunk.join('\n'))
+            fs.appendFileSync('./output/error-skus.csv', csvContArr[j].sku+'\n')
         }
     }
 }
